0f54334b62ec981ce2ffe66ac58f2063005eabdf
Matan Noam Shavit <matannoam@gmail.com>
2017-02-13 20:52:05 -0500

    test required field

--- tests/api/test_orders.py
+++ tests/api/test_orders.py
@@ -3,6 +3,7 @@ from mock import patch
 from nose.tools import eq_
 
 from liedown.api.orders import OrdersResource
+from liedown.api.exceptions import ApiException
 
 
 @patch('liedown.api.orders.request')
@@ -16,5 +17,15 @@ class OrdersTests(TestCase):
 
         response, status_code = self.resource.post()
 
-        eq_(response, {})
         eq_(status_code, 201)
+        eq_(response, {})
+
+    def test_post_shipping_address_requires_zip_code(self, mock_request):
+        mock_request.json = {'shipping_address': {}}
+
+        with self.assertRaises(ApiException) as context:
+            self.resource.post()
+
+        eq_(context.exception.errors,
+            {'shipping_address':
+                {'zip_code': [u'Missing data for required field.']}})
